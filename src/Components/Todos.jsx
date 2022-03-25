87273beef1fae8b6116bd8254ce1898910f02277
akhiladepu <akhil9394021176@gmail.com>
2022-03-25 20:52:53 +0530

    pushign to git, for backup

---
+++ src/Components/Todos.jsx
@@ -0,0 +1,36 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addTodo } from "../Redux/actions";
+
+export const Todos = () => {
+    const [text, setText] = useState("");
+    const todos = useSelector(store => store.todos);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        getTodos()
+    }, [])
+
+    const getTodos = () => {
+        axios.get("https://shielded-headland-23335.herokuapp.com/todos").then((res) => {
+            dispatch(addTodo([...res.data]));
+        })
+    }
+
+    const addTodos = () => {
+        axios.post("https://shielded-headland-23335.herokuapp.com/todos", {
+            title: text
+        }).then(() => { getTodos() });
+    }
+
+    //delete todos
+
+    return (<div>
+        <input type="text" onChange={(e) => { setText(e.target.value) }} />
+        <button onClick={() => { addTodos() }}>Add Todo</button>
+        <div>
+            {todos.map(el => <div>{el.title}</div>)}
+        </div>
+    </div>);
+}
